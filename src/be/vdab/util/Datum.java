f93ffc1d97dd6b9f856fa4406018840ed7fdc4f6
Dekleermaeker Peter <darR3Ke@gmail.com>
2014-08-12 11:31:28 +0200

    Toevoegen van datum validatie + throwen van exceptions

--- src/be/vdab/util/Datum.java
+++ src/be/vdab/util/Datum.java
@@ -11,52 +11,76 @@ public class Datum implements IDatum,
     private int jaar;
     
     /**
-     *  de 99 is voor maand 0, deze gaat alleen maar aangesproken worden bij 
-     *  een initializatie zonder parameters.
+     * array met de maximaal aantal dagen per maand. <p>
+     * de 99 is voor maand 0, deze gaat alleen maar aangesproken worden bij 
+     * een initializatie zonder parameters. <p>
+     * februari heeft 29 dagen + de waarde in schrikkel.
      */
     private int[] maxDag = { 99,31,29,31,30,31,30,31,31,30,31,30,31};
+    
+    /**
+     * variable om te helpen in de berekening van de maand februari voor
+     * schrikkeljaren.<p>
+     * 0 is een schrikkeljaar <p>
+     * -1 is GEEN schrikkeljaar <p>
+     */
     private int schrikkel;
     
+    /**
+     * Initializatie contructor zonder waardes
+     * init waarde = 1/1/1584
+     */
     public Datum(){
         setJaar(1584);        
         setMaand(1);        
         setDag(1);
     }
     
+    /**
+     * constructor om een datum op te vullen
+     * @param dag integer
+     * @param maand integer
+     * @param jaar integer
+     */
     public Datum(int dag, int maand, int jaar) {
         setJaar(jaar);        
         setMaand(maand);        
         setDag(dag);
-
-
     }
-    
+
+    /**
+     * Setter voor dag
+     * @param dag integer
+     */
     @Override
     public void setDag(int dag) {
-        if (!((jaar%4 == 0) && (jaar%100 != 0)))
-            if (!(jaar%400 == 0))
-                schrikkel = -1;
-        else
-                schrikkel = 0;
-         if (dag >= 1 && dag <= maxDag[maand]+schrikkel)
-            this.dag = dag;
-        else
-            System.err.println(dag + "/" + maand + "/" + jaar + " fout");
-
+        this.dag = dag;
     }
     
+    /**
+     * setter voor maand
+     * @param maand integer
+     */
     @Override
     public void setMaand(int maand){
-        if (maand >= 1 && maand <= 12)
         this.maand = maand;
     }
     
+    /**
+     * setter voor jaar
+     * @param jaar integer
+     */
     @Override
     public void setJaar(int jaar){
-        if (jaar >= 1584 && jaar <= 4099)
             this.jaar = jaar;
     }
     
+    /**
+     * setter voor de volledige datum
+     * @param dag integer
+     * @param maand integer
+     * @param jaar integer
+     */
     @Override
     public void setDatum(int dag, int maand, int jaar){
         setDag(dag);
@@ -64,26 +88,48 @@ public class Datum implements IDatum,
         setJaar(jaar);
     }
     
+    /**
+     * getter voor dag
+     * @return dag integer
+     */
     @Override
     public int getDag(){
         return dag;
     }
     
+    /**
+     * getter voor maand
+     * @return maand integer
+     */
     @Override
     public int getMaand(){
         return maand;
     }
     
+    /**
+     * getter voor jaar
+     * @return jaar integer
+     */
     @Override 
     public int getJaar(){
         return jaar;
     }
     
+    /**
+     * Override voor string waarde van de class datum
+     * @return dd/mm/jjjj
+     */
     @Override
     public String toString(){
         return dag + "/" + maand + "/" + jaar;
     }
     
+    /**
+     * Comparable van datum
+     * @param d Object
+     * @return  0 is gelijk
+     *          -1 is niet gelijk
+     */
     @Override
     public int compareTo(Object d){       // Generics ? Object --> Datum
         if (dag == ((Datum)d).getDag() &&
@@ -92,6 +138,26 @@ public class Datum implements IDatum,
             return 0;
         else
             return -1;
+    }
+    
+    private Boolean DatumValidatie() throws DatumException{
+        
+        if (!(jaar >= 1584 && jaar <= 4099))
+            throw new DatumException("Jaar niet in de geldige range(1584-4099)",jaar);
+        
+        if (!(maand >= 1 && maand <= 12))
+            throw new DatumException("Maand niet in de geldige range(1-12)",maand);
+        
+        if (!((jaar%4 == 0) && (jaar%100 != 0)))
+            if (!(jaar%400 == 0))
+                schrikkel = -1;
+        else
+                schrikkel = 0;
+         if (!(dag >= 1 && dag <= maxDag[maand]+schrikkel))
+             throw new DatumException("dag niet in de geldige range (1-"+maxDag[maand]+schrikkel+")",dag);
+
+         return true;
+
     }
     
 }
