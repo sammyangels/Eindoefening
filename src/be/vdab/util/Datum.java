9330f73608bc2428a5c25007a84a7be18bdfec79
Dekleermaeker Peter <darR3Ke@gmail.com>
2014-08-12 10:53:24 +0200

    Eerste basis versie. controle schrikkeljaar werkt
    Uitbreiden controle datum

--- src/be/vdab/util/Datum.java
+++ src/be/vdab/util/Datum.java
@@ -4,22 +4,45 @@ package be.vdab.util;
  *
  * @author Dekleermaeker Peter
  */
-public class Datum implements IDatum {
+public class Datum implements IDatum, 
+                              Comparable{
     private int dag;
     private int maand;
     private int jaar;
     
+    /**
+     *  de 99 is voor maand 0, deze gaat alleen maar aangesproken worden bij 
+     *  een initializatie zonder parameters.
+     */
+    private int[] maxDag = { 99,31,29,31,30,31,30,31,31,30,31,30,31};
+    private int schrikkel;
+    
+    public Datum(){
+        setJaar(1584);        
+        setMaand(1);        
+        setDag(1);
+    }
+    
     public Datum(int dag, int maand, int jaar) {
+        setJaar(jaar);        
+        setMaand(maand);        
         setDag(dag);
-        setMaand(maand);
-        setJaar(jaar);
-        
+
+
     }
     
     @Override
     public void setDag(int dag) {
-        if (dag >= 1 && dag <= 31) // uitbreiden
+        if (!((jaar%4 == 0) && (jaar%100 != 0)))
+            if (!(jaar%400 == 0))
+                schrikkel = -1;
+        else
+                schrikkel = 0;
+         if (dag >= 1 && dag <= maxDag[maand]+schrikkel)
             this.dag = dag;
+        else
+            System.err.println(dag + "/" + maand + "/" + jaar + " fout");
+
     }
     
     @Override
@@ -55,4 +78,20 @@ public class Datum implements IDatum {
     public int getJaar(){
         return jaar;
     }
+    
+    @Override
+    public String toString(){
+        return dag + "/" + maand + "/" + jaar;
+    }
+    
+    @Override
+    public int compareTo(Object d){       // Generics ? Object --> Datum
+        if (dag == ((Datum)d).getDag() &&
+                maand == ((Datum)d).getMaand() &&
+                jaar == ((Datum)d).getJaar())
+            return 0;
+        else
+            return -1;
+    }
+    
 }
